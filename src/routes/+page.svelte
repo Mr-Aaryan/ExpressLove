6435826101b445944dee602b4a465ccf3059db66
maheshrokaya0 <maheshrokaya6647@gmail.com>
2024-02-07 16:24:23 +0545

    hello

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
 <h1>Welcome to SvelteKit</h1>
 <p class="text-5xl text-sky-400">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<h1 class="text-black">Hello aaryan fucker</h1>
+<h1 class="text-black">Hello world</h1>
