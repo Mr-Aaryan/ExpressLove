ff20fb8ae306bdc07587abb239082e2643b61bba
Mr-Aaryan <kc.aryan3536@gmail.com>
2024-02-08 14:52:53 +0545

    card1 finish

---
+++ src/lib/components/main/afterHome.svelte
@@ -0,0 +1,36 @@
+
+<script>
+    import {createEventDispatcher} from 'svelte';
+
+    import hearts from '$lib/images/hearts.png';
+    import yesPlease from '$lib/images/yesPlease.gif';
+    
+    let box, alertBox;
+
+    const dispatch = createEventDispatcher();
+
+    function handleYesClick(){
+        dispatch('yesClick');
+    }
+
+    function handleNoClick(){
+        dispatch('noClick');
+    }
+
+</script>
+
+    <div class="left-0 right-0 ml-auto mr-auto absolute w-[36rem] mx-auto mt-16 shadow-md shadow-[#f3616d3b] bg-[#f3616d3b] mix-blend-lighten rounded-tl-3xl rounded-br-3xl">
+        <div class="mt-5">
+            <img src="{yesPlease}" alt="Yes, Please" class="mx-auto h-96 ">
+        </div>
+        <div class="border border-[#ffffff66] w-10/12 mx-auto my-7">
+            <div class="w-fit bg-white px-3 py-2 mx-auto my-5 text-[#E30346] rounded-3xl text-2xl font-medium">Will you go on a date with me?</div>
+            <div class="flex justify-between w-10/12 mx-auto my-7">
+                <div class="border-2 px-6 py-3 bg-[#E30346] text-xl text-white rounded-full hover:cursor-pointer" on:click={handleYesClick} >Yes</div>
+                <!-- svelte-ignore a11y-click-events-have-key-events -->
+                <!-- svelte-ignore a11y-no-static-element-interactions -->
+                <div class="border-2 px-6 py-3 bg-[#E30346] text-xl text-white rounded-full hover:cursor-pointer" on:click={handleNoClick}>No</div>
+            </div>
+        </div>
+    </div>
+
--- src/lib/components/main/home.svelte
+++ src/lib/components/main/home.svelte
@@ -1,15 +1,28 @@
 
 <script>
-
+    import {createEventDispatcher} from 'svelte';
     import hearts from '$lib/images/hearts.png';
+    
+    //click dispatcher
+    const dispatch = createEventDispatcher();
 
+    function handleClick(){
+        dispatch('click');
+    }
+
+    let box, alertBox;    
+
+    //alert
     function myAlert(){
-        window.alert("Sorry this option isn't available. Please try again.");
+        box.classList.add("blur-md");
+        alertBox.classList.remove("hidden");
+    }
+    function alertRem(){
+        box.classList.remove("blur-md");
+        alertBox.classList.add("hidden");
     }
 </script>
-
-<main class="bg-[#F3616E] h-screen border border-[#F3616E]">
-    <div class="w-[36rem] mx-auto mt-16 border ">
+    <div class="left-0 right-0 ml-auto mr-auto absolute w-[36rem] mx-auto mt-16 shadow-md shadow-[#f3616d3b] bg-[#f3616d3b] mix-blend-lighten rounded-tl-3xl rounded-br-3xl" bind:this={box}>
         <div>
             <div >
                 <div class="heading flex align-center w-fit mx-auto">
@@ -28,11 +41,28 @@
         <div class="border border-[#ffffff66] w-10/12 mx-auto my-7">
             <div class="w-fit bg-white px-3 py-2 mx-auto my-5 text-[#E30346] rounded-3xl text-2xl font-medium">Will you be my Valentine?</div>
             <div class="flex justify-between w-10/12 mx-auto my-7">
-                <div class="border-2 px-6 py-3 bg-[#E30346] text-xl text-white rounded-full hover:cursor-pointer" >Yes</div>
+                <!-- svelte-ignore a11y-click-events-have-key-events -->
+                <!-- svelte-ignore a11y-no-static-element-interactions -->
+                <div class="border-2 px-6 py-3 bg-[#E30346] text-xl text-white rounded-full hover:cursor-pointer" on:click={handleClick} >Yes</div>
                 <!-- svelte-ignore a11y-click-events-have-key-events -->
                 <!-- svelte-ignore a11y-no-static-element-interactions -->
                 <div class="border-2 px-6 py-3 bg-[#E30346] text-xl text-white rounded-full hover:cursor-pointer" on:click={myAlert}>No</div>
             </div>
         </div>
     </div>
-</main>
+    <div class="hidden absolute bg-[#504b4b] text-2xl text-white w-[32rem] top-1/3 left-52 border shadow-md shadow-black" bind:this={alertBox}>
+        <div class="w-fit mx-auto my-2">
+            <div>Sorry this option is not available.</div>
+            <div>Please try again !!</div>
+        </div>
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <!-- svelte-ignore a11y-no-static-element-interactions -->
+        <div class="w-fit mx-auto my-6 bg-[#2b2929] py-3 px-5 border cursor-pointer" on:click={alertRem} >
+            <button>OK</button>
+        </div>
+    </div>
+<!-- <style>
+    .hi{
+        color: #ff00003d;
+    }
+</style> -->
