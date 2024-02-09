c50afff78378df5d1fb744f73b4e79182d1f14c8
Mr-Aaryan <kc.aryan3536@gmail.com>
2024-02-10 02:06:03 +0545

    responsive

--- src/lib/design/box.svelte
+++ src/lib/design/box.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <Layout>
-    <div class="sm:p-5 overflow-auto h-screen scroll-smooth">
-        <div class="max-w-[36rem] mx-auto sm:mt-16 bg-[#f3616d3b] rounded-lg p-5 h-full sm:h-auto overflow-scroll absolute top-0 left-0 right-0 sm:relative scroll-smooth">
+    <div class="sm:p-5 h-screen overflow-auto scroll-smooth">
+        <div class="max-w-[36rem] mx-auto sm:mt-16 bg-[#f3616d3b] rounded-lg p-5 h-screen sm:h-auto overflow-scroll absolute top-0 left-0 right-0 sm:relative scroll-smooth">
             <slot />
         </div>
     </div>
--- src/routes/rejection/+page.svelte
+++ src/routes/rejection/+page.svelte
@@ -9,12 +9,6 @@
         <img src="{noPlease}" alt="No, Please" class="mx-auto h-72 sm:h-96 rounded">
     </div>
     <div class="w-10/12 mx-auto mt-5 py-8">
-        <p class="no_text text-white text-center">Thanks for being so up front. I’m disappointed of course, but I totally respect your decision.</p>
+        <p class="text-white text-center">Thanks for being so up front. I’m disappointed of course, but I totally respect your decision.</p>
     </div>
 </Box>
-
-<style>
-    .no_text{
-        text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #FFF;
-    }
-</style>
