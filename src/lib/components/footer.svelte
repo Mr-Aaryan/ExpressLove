b62d9db0286934ee7c15ce5f4066a1d0a52c9245
Mr-Aaryan <kc.aryan3536@gmail.com>
2024-02-10 21:09:46 +0545

    removed footer

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,6 +1,6 @@
-<footer class="">
+<!-- <footer class="">
     <div class="py-5 text-center mt-20">
         <a href="https://www.buymeacoffee.com/express_love" target="_blank" class=" underline hover:text-sky-600 block mb-2">Buy me a ☕coffee</a>
         <p>Built with ❤️ by <a href="https://github.com/Mr-Aaryan" class="text-sky-600 hover:underline">@aaryan</a></p>
     </div>
-</footer>
+</footer> -->
