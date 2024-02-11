cc5920df1f67d77fd60e929c65401d01d1a6e7a6
Mr-Aaryan <kc.aryan3536@gmail.com>
2024-02-11 16:25:53 +0545

    gooter

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -4,6 +4,6 @@
             <a href="/privacy-policy" class="inline-block underline hover:text-sky-600">Privacy</a>
             <a href="https://www.buymeacoffee.com/express_love" target="_blank" class=" underline hover:text-sky-600 mb-2 inline-block ml-3">Buy me a ☕coffee</a>
         </div>
-        <p>Built with ❤️ by <a href="https://github.com/Mr-Aaryan" class="text-sky-600 hover:underline">@aaryan</a></p>
+        <p>Built with ❤️ by @aaryan</p>
     </div>
 </footer>
