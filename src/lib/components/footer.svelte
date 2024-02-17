f152b1b428c04bca5ff0dda99d91f9364d0d8b5f
Mr-Aaryan <kc.aryan3536@gmail.com>
2024-02-17 13:22:32 +0545

    footer

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -2,7 +2,7 @@
     <div class="py-5 text-center mt-20 ">
         <div>
             <a href="/privacy-policy" class="inline-block underline hover:text-sky-600">Privacy</a>
-            <a href="https://www.buymeacoffee.com/express_love" target="_blank" class=" underline hover:text-sky-600 mb-2 inline-block ml-3">Buy me a ☕coffee</a>
+            <!-- <a href="https://www.buymeacoffee.com/express_love" target="_blank" class=" underline hover:text-sky-600 mb-2 inline-block ml-3">Buy me a ☕coffee</a> -->
         </div>
         <p>Built with ❤️ by @aaryan</p>
     </div>
