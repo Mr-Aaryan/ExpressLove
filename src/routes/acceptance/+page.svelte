a012151c64b58cf02b4e8a3c85ee5bc3eb2111a2
Mr-Aaryan <kc.aryan3536@gmail.com>
2025-02-03 22:09:27 +0545

    add: updated msg

--- src/routes/acceptance/+page.svelte
+++ src/routes/acceptance/+page.svelte
@@ -12,6 +12,6 @@
         <img src="{yesPlease}" alt="Yes, Please" class="mx-auto h-72 sm:h-96 rounded">
     </div>
     <div class=" mx-auto mt-4">
-        <p class="text-[white] sm:text-xl px-5 py-4 rounded-xl">My love, accepting my proposal means the world to me. Your love and acceptance have filled my heart with indescribable joy. I am grateful for your trust and for believing in our love and the beautiful future, we can create together. I promise to love you unconditionally, support you in all your endeavors, and cherish every moment we share. Thank you for saying yes and embarking on this extraordinary journey of love with me.</p>
+        <p class="text-[white] sm:text-xl px-5 py-4 rounded-xl">I love you, and I promise to make every day with you special.</p>
     </div>
 </Box>
