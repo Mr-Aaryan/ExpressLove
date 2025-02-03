45095897085af926946456bdb19b8daaa8f78300
Mr-Aaryan <kc.aryan3536@gmail.com>
2025-02-03 16:06:01 +0545

    updated

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,9 +1,15 @@
 <footer class="">
-    <div class="py-5 text-center mt-20 ">
-        <div>
-            <a href="/privacy-policy" class="inline-block underline hover:text-sky-600">Privacy</a>
-            <!-- <a href="https://www.buymeacoffee.com/express_love" target="_blank" class=" underline hover:text-sky-600 mb-2 inline-block ml-3">Buy me a ☕coffee</a> -->
-        </div>
-        <p>Built with ❤️ by @aaryan</p>
+  <div class="py-5 text-center mt-20">
+    <div>
+      <a
+        href="/privacy-policy"
+        class="inline-block underline hover:text-sky-600">Privacy</a
+      >
+      <!-- <a href="https://www.buymeacoffee.com/express_love" target="_blank" class=" underline hover:text-sky-600 mb-2 inline-block ml-3">Buy me a ☕coffee</a> -->
     </div>
+    <p>
+      Built with ❤️ by
+      <a href="https://github.com/Mr-Aaryan" class="underline">@aaryan</a>
+    </p>
+  </div>
 </footer>
--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,12 +1,21 @@
 <script>
-    import * as config from '$lib/config';
+  import * as config from "$lib/config";
 </script>
+
 <header class="w-full border-b px-5">
-    <div class="h-16 flex items-center max-w-4xl mx-auto flex justify-between">
-        <a href="/" class="text-2xl text-red-600 font-semibold">{ config.siteTitle }</a>
-        <div class="">
-            <a href="/" class="ml-3 hover:text-sky-600">Home</a>
-            <a href="/pickup-lines" class="ml-3 hover:text-sky-600">Pickup lines</a>
-        </div>
+  <div class="max-w-4xl mx-auto flex items-center justify-center gap-3 border">
+    <p>New Domain:</p>
+    <a href="https://expresslove.pages.dev" class="text-sky-600"
+      >expresslove.pages.dev</a
+    >
+  </div>
+  <div class="h-16 flex items-center max-w-4xl mx-auto justify-between">
+    <a href="/" class="text-2xl text-red-600 font-semibold"
+      >{config.siteTitle}</a
+    >
+    <div class="">
+      <a href="/" class="ml-3 hover:text-sky-600">Home</a>
+      <a href="/pickup-lines" class="ml-3 hover:text-sky-600">Pickup lines</a>
     </div>
+  </div>
 </header>
