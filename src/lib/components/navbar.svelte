52bd8f98ab91d70eb5ea30624721a1cb6f185b36
Mr-Aaryan <kc.aryan3536@gmail.com>
2026-02-08 16:12:35 +0545

    Comment out new domain section in navbar
    Comment out the section displaying the new domain link.

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -3,12 +3,12 @@
 </script>
 
 <header class="w-full border-b px-5">
-  <div class="max-w-4xl mx-auto flex items-center justify-center gap-3 border">
-    <p>New Domain:</p>
-    <a href="https://expresslove.vercel.app" class="text-sky-600"
-      >expresslove.vercel.app</a
-    >
-  </div>
+ // <div class="max-w-4xl mx-auto flex items-center justify-center gap-3 border">
+ //   <p>New Domain:</p>
+ //   <a href="https://expresslove.vercel.app" class="text-sky-600"
+ //     >expresslove.vercel.app</a
+ //   >
+ // </div>
   <div class="h-16 flex items-center max-w-4xl mx-auto justify-between">
     <a href="/" class="text-2xl text-red-600 font-semibold"
       >{config.siteTitle}</a
